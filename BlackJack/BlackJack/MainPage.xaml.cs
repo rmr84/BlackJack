52975fd13888f9e9767f1b04ae6af2445e13a921
rmr84 <73509790+rmr84@users.noreply.github.com>
2021-11-30 15:25:36 -0500

    Add project files.

---
+++ BlackJack/BlackJack/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BlackJack
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewStats_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tutorial_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
