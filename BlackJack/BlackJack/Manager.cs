74d9ead539ec35570378af15a84a251ea2b4feea
rmr84 <73509790+rmr84@users.noreply.github.com>
2021-12-02 14:32:04 -0500

    added data manager for web api

---
+++ BlackJack/BlackJack/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace BlackJack
+{
+    class Manager
+    {
+        public static readonly Manager manager = new Manager();
+        public List<APIModel> list = new List<APIModel>();
+        
+
+        private Manager() { }
+
+        public static Manager GetInstance()
+        {
+            return manager;
+        }
+
+        public List<APIModel> GetItems()
+        {
+            return list;
+        }
+
+        public ObservableCollection<APIModel> GetObsList()
+        {
+            return new ObservableCollection<APIModel>();
+        }
+    }
+}
