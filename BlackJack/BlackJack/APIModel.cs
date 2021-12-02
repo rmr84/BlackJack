a08274c6c53537d0aa298ca12c500d8d33f37368
rmr84 <73509790+rmr84@users.noreply.github.com>
2021-12-01 19:50:28 -0500

    background and api controller

---
+++ BlackJack/BlackJack/APIModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack
+{
+     public class APIModel
+    {
+        public string userName { get; set; }
+        public int wins { get; set; }
+        public int losses { get; set; }
+        public int handsPlayed { get; set; }
+        
+        public int blackjacks { get; set; }
+        public int dealerBlackjacks { get; set; }
+        public int busts { get; set; }
+
+        public APIModel() { }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+
+
+    }
+}
